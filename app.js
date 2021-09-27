fb6ec1de3f290a63f2a2dee36119ce8b7267c8d4
KamranMoazim <kamrannaseer765@gmail.com>
2021-09-27 20:15:29 +0500

    graphql and mongodb

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlHTTP} = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
 const PORT = 5000;
+const Event = require('./models/event');
 
 const app = express();
 
-const events = [];
+// const events = [];
 
 app.use(bodyParser.json());
 
@@ -44,23 +46,59 @@ app.use(
         }
     `),
     rootValue: {
-      events: () => {
-        return events;
-      },
+    //   events: () => {
+    //     return events;
+    //   },
+    //   createEvent: args => {
+    //     const event = {
+    //       _id: Math.random().toString(),
+    //       title: args.eventInput.title,
+    //       description: args.eventInput.description,
+    //       price: args.eventInput.price?args.eventInput.price:0,
+    //       date: Date.now().toString()
+    //     };
+    //     events.push(event);
+    //     return event;
+    //   }
+    events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+        })
+          .catch(err => {
+            throw err;
+        });
+    },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: args.eventInput.price?args.eventInput.price:0,
-          date: Date.now().toString()
-        };
-        events.push(event);
-        return event;
+          date: args.eventInput.date? new Date(args.eventInput.date) : Date.now().toString(), 
+        });
+        return event.save()
+          .then(result => {
+            // console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+        })
+          .catch(err => {
+            console.log(err);
+            throw err;
+        });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(PORT,()=>console.log(`Server is listening at port ${PORT}`));
+mongoose.connect("mongodb://localhost:27017/graphQL", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    app.listen(PORT,()=>console.log(`Server is listening at port ${PORT}`));
+    console.log("DB CONNECTED")
+}).catch(()=>{
+    console.log("ERROR")
+})
